d51b94f728837a89551da937d05688dd19688ccb
MichalLenar <31937@student.pwsztar.edu.pl>
2019-10-25 16:06:53 +0200

    Moze dziala

---
+++ IdeaProjects/PierwszyHTML/js/script.js
@@ -0,0 +1,9 @@
+let i;
+let j;
+for (i=1; i<=10; i++)
+
+  for (j = 1; j <= 10; j++) {
+    let wynik;
+    wynik = i * j;
+    console.log(wynik);
+  }
---
+++ IdeaProjects/PierwszyHTML/js/scroll.js
@@ -0,0 +1,14 @@
+let button = document.querySelector('.scroll');
+button.addEventListener('click', goToTop);
+function goToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+  document.querySelector(".scroll").style.display = "block";
+} else {
+  document.querySelector(".scroll").style.display = "none";
+}
+window.onscroll = function() {showTopButton()};
+
+
